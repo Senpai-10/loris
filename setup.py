3a430f439d762dad683572dbbdfe7ca103f9a9f9
abdalrzag safealden <53495583+Senpai-10@users.noreply.github.com>
2020-12-09 10:04:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,36 +4,17 @@ from setuptools.command.test import test as TestCommand
 
 
 setup(
-    name='pypi-cli',
-    version=__version__,
-    description='A command-line interface to the Python Package Index (PyPI).',
-    long_description=(read('README.rst') + '\n\n' +
-                      read('CHANGELOG.rst')),
-    author='Steven Loria',
-    author_email='sloria1@gmail.com',
-    url='https://github.com/sloria/pypi-cli',
-    install_requires=REQUIRES,
-    license=read("LICENSE"),
-    zip_safe=False,
+    name='loris',
+    version=1.0.0,
+    description='',
+    author='senpai-10',
+    url='https://github.com/Senpai-10/loris',
+    install_requires=[],
     include_package_data=True,
-    keywords='pypi cli command line pipstat pip statistics download count metrics analytics',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-    ],
-    py_modules=["pypi_cli"],
+    py_modules=["loris"],
     entry_points={
         'console_scripts': [
-            "pypi = pypi_cli:cli"
+            "loris = loris:main"
         ]
-    },
-    tests_require=TESTS_REQUIRE,
-    cmdclass={'test': PyTest}
+    }
 )
