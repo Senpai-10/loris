adf4a4aacd226fc09481afbfc9bbe434bb45d653
abdalrzag safealden <53495583+Senpai-10@users.noreply.github.com>
2020-12-09 10:07:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='',
     author='senpai-10',
     url='https://github.com/Senpai-10/loris',
-    install_requires=[],
+    install_requires=['click>=3.0'],
     include_package_data=True,
     py_modules=["loris"],
     entry_points={
